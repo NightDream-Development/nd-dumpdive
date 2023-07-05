f1165fba18084f5fddf6ea943ecc80363954e1fe
GoodbananaHUN <73757004+GoodbananaHUN@users.noreply.github.com>
2023-07-05 02:25:21 +0200

    Add files via upload

---
+++ nd-dumping/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+auth 'GoodbananHUN  dc:  .goodbanana'
+
+client_script 'client/*.lua'
+server_script 'server/*.lua'
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'config.lua'
+}
